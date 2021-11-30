cf9b3cc4f528e3710caadcd35088982f63aa7cd3
Linda Lätzsch <laetzsch@hochschule-bc.de>
2021-11-30 10:01:15 +0100

    Add bash script

---
+++ count_lines.sh
@@ -0,0 +1,6 @@
+# Ein kleines Script zum Zählen von Zeilen in TSV-Dateien
+# 2021 - Linda Lätzsch <laetzsch@hochschule-bc.de>
+echo "Count lines in CSV files"
+wc -l *tsv
+echo "Done! Have a nice day!"
+
